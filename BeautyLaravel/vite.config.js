7f90968ff15ac39b1b0ddef559d7c8ac2121cda7
unknown <aigor2407@gmail.com>
2024-04-26 13:45:44 -0300

    ajustes nas imagens

--- BeautyLaravel/vite.config.js
+++ BeautyLaravel/vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/css/style.css', 'resources/css/montarCronograma.css', 'resources/css/capa.css', 'resources/css/crud.css', 
+            input: ['resources/css/app.css', 'resources/css/pagina2.css', 'resources/css/montarCronograma.css', 'resources/css/capa.css', 'resources/css/crud.css', 
             'resources/css/crudCliente.css', 'resources/css/terceiraTela', 'resources/js/app.js'],
             refresh: true,
         }),
